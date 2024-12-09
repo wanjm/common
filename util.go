854ef3c79c5033022f0d3d3059a581b12a80c770
wanjm <wanjm@mac-pro.plaso>
2024-12-09 14:56:44 +0800

    add HmacSha1

--- util.go
+++ util.go
@@ -1,9 +1,19 @@
 package common
 
-import "context"
+import (
+	"context"
+	"crypto/hmac"
+	"crypto/sha1"
+	"encoding/hex"
+)
 
 func Recover(ctx context.Context, message string) {
 	if r := recover(); r != nil {
 		Error(ctx, "panic in go routine", String("callInfo", message))
 	}
+}
+func HmacSha1(keyStr string, message string) string {
+	mac := hmac.New(sha1.New, []byte(message))
+	mac.Write([]byte([]byte(keyStr)))
+	return hex.EncodeToString(mac.Sum(nil))
 }
