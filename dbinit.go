0411c799bf4eb5c66f534a42a57d4c0afeded89e
wan_jm <wan_jm@126.com>
2026-03-22 17:19:28 +0800

    add rabbit and redis

--- dbinit.go
+++ dbinit.go
@@ -1,13 +1,16 @@
 package common
 
 import (
+	"github.com/redis/go-redis/v9"
 	"go.mongodb.org/mongo-driver/mongo"
 	"gorm.io/gorm"
 )
 
 type DbConfig struct {
-	MySqlConfig MySqlConfig
-	MongoConfig MongoConfig
+	MySqlConfig    MySqlConfig
+	MongoConfig    MongoConfig
+	RedisConfig    RedisConfig
+	RabbitMqConfig RabbitMqConfig
 }
 
 var Cfg *DbConfig
@@ -18,12 +21,27 @@ func ConnectMysqlDB() (mysqlDB *gorm.DB) {
 }
 
 // @gos type="initiator"
-func GetMongoClient() (mongoDB *mongo.Client) {
+func GetMongoClient() (mongo *mongo.Client) {
 	return ConnectMongo(&Cfg.MongoConfig)
 }
 
 // @gos type="initiator"
-func ConnectMongoDB(mongo *mongo.Client) (mongoDb *mongo.Database) {
+func ConnectMongoDB(mongo *mongo.Client) (mongoDB *mongo.Database) {
 	db := mongo.Database(Cfg.MongoConfig.Database)
 	return db
+}
+
+// @gos type="initiator"
+func ConnectRedisClient() (redis *redis.Client) {
+	return ConnectRedis(&Cfg.RedisConfig)
+}
+
+// @gos type="initiator"
+func ConnectRabbitConsumer() (normalConsumer *RabbitMqClient) {
+	return ConnectConsumer(&Cfg.RabbitMqConfig)
+}
+
+// @gos type="initiator"
+func ConnectRabbitFanoutConsumer() (fanoutConsumer *RabbitMqClient) {
+	return ConnectFanoutConsumer(&Cfg.RabbitMqConfig)
 }
