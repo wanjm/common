114f3f97b1a01ea845921d2789c5753e92e07026
wan_jm <wan_jm@126.com>
2026-03-28 22:05:50 +0800

    add PageInfo to common as autocode gen need it

--- class.go
+++ class.go
@@ -1,12 +1,12 @@
 package common
 
-// DefaultPageSize is used when request PageSize is missing or <= 0.
+// DefaultPageSize is used when request PageSize is missing or < 0.
 const DefaultPageSize = 20
 
 // PageInfo is embedded in list requests for pagination (JSON: pageNo, pageSize).
 type PageInfo struct {
-	PageNum  int `json:"pageNum"`
-	PageSize int `json:"pageSize"`
+	PageNum  int `json:"pageNum"`  // 页码,从0开始;
+	PageSize int `json:"pageSize"` // 每页条数,小于0时使用DefaultPageSize;=0表示全部
 }
 
 // NormalizedPageSize returns PageSize or DefaultPageSize when zero or negative.
